2df9c8806293fd4f281857723970aeba5e8beae2
pozgerab <74205032+pozgerab@users.noreply.github.com>
2022-06-23 16:49:04 +0200

    Created customentities project

---
+++ mc/mod/CustomEntitiesPlugin/src/main/java/com/gertoxq/customentities/customentities.java
@@ -0,0 +1,17 @@
+package com.gertoxq.customentities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class customentities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
